9de95a4e55d5c3425422d5d9baf4fbc679069b52
leedonghyun79 <pixelconnect79@gmail.com>
2025-10-28 23:57:51 +0900

    chore: 불필요한 코드 제거

--- server/src/common/swagger.ts
+++ server/src/common/swagger.ts
@@ -1,12 +1,10 @@
 import {
   OpenApiGeneratorV3,
-  OpenAPIRegistry,
 } from "@asteasolutions/zod-to-openapi";
 import { registry } from "../domain/recruits/recruits.schema.js";
 
 const generator = new OpenApiGeneratorV3(registry.definitions);
 console.log("registry.definitions:", registry.definitions); // 문서 정의한 객체들이 담김
-const components = generator.generateComponents();
 
 export const swaggerDocument = generator.generateDocument({
   openapi: "3.0.0",
@@ -16,4 +14,5 @@ export const swaggerDocument = generator.generateDocument({
     description: "Groother API 입니다.",
     version: "1.0.0",
   },
+  
 });
